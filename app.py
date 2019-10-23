d1b65978b42b1b75995a325d8d69f558ee710cc0
patricia1387 <32620335+patricia1387@users.noreply.github.com>
2019-10-23 09:14:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello OpenShift, it's wednesday!"
+    return "Hello OpenShift!"
 
 if __name__ == "__main__":
     application.run()
