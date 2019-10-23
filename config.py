2bb9f22f922612225392afd8210bce2e6c11d15c
Patricia Hudecova <xhudec30@stud.fit.vutbr.cz>
2019-10-23 11:54:13 +0200

    added variable

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
+os.environ['API_GREETINGS'] = 'greetings'
 
+# Get environment variables
+GREETINGS = os.getenv('API_GREETINGS')
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
